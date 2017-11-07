b78695da6e7a205b36d43c5eb36760c92989d5cb
Joshua Hatcher <joshua.abraham.hatcher@gmail.com>
2017-11-07 15:28:30 -0500

    Submodule sync

--- update_submodule.sh
+++ update_submodule.sh
@@ -1,12 +1,15 @@
+BLUE='^[[34m'
+RED='^[[31m'
+
 update_submodules() {
   git submodule update --remote --merge
   git add .
   git commit -m 'Submodule sync'
-  echo "Submodules updated and committed. Run 'git push origin' if you want."
+  echo -e "${BLUE}Submodules updated and committed. Run 'git push origin' if you want."
 }
 
 while true; do
-  read -p "You are about to commit all updates to this repo's git submodules. Are you sure you want to proceed? (y/n)" yn
+  read -p "${BLUE}You are about to commit all updates to this repo's git submodules. ${RED}Are you sure you want to proceed? (y/n)" yn
   case $yn in
     [Yy]* ) update_submodules; break;;
     [Nn]* ) exit;;
